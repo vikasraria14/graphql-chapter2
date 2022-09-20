5559842f1864e0df2be6e032efdc33a7959c5d29
vikasraria14 <38049874+vikasraria14@users.noreply.github.com>
2022-09-20 17:34:24 +0530

    Getting error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from '@apollo/client'
 
-function App() {
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+
+import { ALL_PERSONS } from './queries'
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Persons persons={result.data.allPersons} />
+      <PersonForm />
+    </>
+  )
 }
 
-export default App;
+export default App
